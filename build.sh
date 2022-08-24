50de448b4cb7d29889cc00bc4b68755c4c7a87ad
Ju Gwangjin <gwangjin@postech.ac.kr>
2022-08-24 18:49:05 +0900

    change make option

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
+echo "cmake"
 cmake .. -DCMAKE_BUILD_TYPE=Release -w
+echo "make"
 make -j -w
 
 cd ../../g2o
@@ -12,7 +14,9 @@ echo "Configuring and building Thirdparty/g2o ..."
 
 mkdir build
 cd build
+echo "cmake"
 cmake .. -DCMAKE_BUILD_TYPE=Release -w
+echo "make"
 make -j -w
 
 cd ../../../
@@ -27,5 +31,7 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
+echo "cmake"
 cmake .. -DCMAKE_BUILD_TYPE=Release  -w
+echo "make"
 make -j -w
